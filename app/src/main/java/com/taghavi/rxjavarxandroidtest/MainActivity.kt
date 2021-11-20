9a53519e7e26edbb5f8510e069ca042443399aaf
EhsanTgv <EhsanTgv@gmail.com>
2021-11-20 15:57:13 +0330

    Implement Observables and Observers

--- app/src/main/java/com/taghavi/rxjavarxandroidtest/MainActivity.kt
+++ app/src/main/java/com/taghavi/rxjavarxandroidtest/MainActivity.kt
@@ -2,10 +2,41 @@ package com.taghavi.rxjavarxandroidtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val taskObservable: Observable<Task> = Observable
+            .fromIterable(DataSource.createTasksList())
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+
+        taskObservable.subscribe(object : Observer<Task> {
+            override fun onSubscribe(d: Disposable?) {
+                Log.d(TAG, "onSubscribe: Called")
+            }
+
+            override fun onNext(task: Task) {
+                Log.d(TAG, "onNext: " + task.description)
+            }
+
+            override fun onError(e: Throwable?) {
+                Log.d(TAG, "onError: $e")
+            }
+            override fun onComplete() {
+                Log.d(TAG, "onComplete: Called")
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/taghavi/rxjavarxandroidtest/Task.kt
@@ -0,0 +1,7 @@
+package com.taghavi.rxjavarxandroidtest
+
+class Task(
+    val description: String,
+    val isComplete: Boolean,
+    val priority: Int
+)
