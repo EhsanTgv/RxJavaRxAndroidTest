576cead1057680930cfedc18cc4f370967070c0f
EhsanTgv <EhsanTgv@gmail.com>
2021-12-11 11:30:28 +0300

    add filter to Observable

--- app/src/main/java/com/taghavi/rxjavarxandroidtest/MainActivity.kt
+++ app/src/main/java/com/taghavi/rxjavarxandroidtest/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.taghavi.rxjavarxandroidtest
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.annotation.RequiresApi
 
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.annotations.NonNull
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 
 import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.function.Predicate
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
@@ -24,6 +28,12 @@ class MainActivity : AppCompatActivity() {
         val taskObservable: Observable<Task> = Observable
             .fromIterable(DataSource.createTasksList())
             .subscribeOn(Schedulers.io())
+            .filter(@RequiresApi(Build.VERSION_CODES.N)
+            object : Predicate<Task>, @NonNull io.reactivex.rxjava3.functions.Predicate<Task> {
+                override fun test(task: Task): Boolean {
+                    return task.isComplete
+                }
+            })
             .observeOn(AndroidSchedulers.mainThread())
 
         taskObservable.subscribe(object : Observer<Task> {
